75f6c35efa7e09d68f26cc1c62180365fb82835d
mobara121 <mizueo121@gmail.com>
2021-02-02 21:12:06 -0500

    ssl: true / db.js

--- db.js
+++ db.js
@@ -2,6 +2,10 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
 })
 
 sequelize.authenticate().then(
